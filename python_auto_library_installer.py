e15ddf3daaccdde5963f16d7eb346f9fb7f0e1d6
NetworkSpirit <sasi0123@outlook.com>
2020-09-06 12:25:28 -0400

    Add files via upload

---
+++ python_auto_library_installer.py
@@ -0,0 +1,31 @@
+﻿import os
+paketler = ["django","kivy","facebook-sdk","google","pygame","pandas","numpy","schedule","opencv-python","scrapy","requests","wxPython","pillow","SQLAlchemy==1.3.15","beautifulsoup4","twisted","scipy","matplotlib","pywin32","nltk","nose"]
+
+print("****************Welcome to Auto Package Installer*********************")
+print("****************Press 1 to Install Packages Automatically**************")
+print("****************Press 2 to Install Packages Manually*******************")
+secim= input("")
+
+if secim =="1":
+	print("**************************************************************")
+	print("*************INSTAGRAM.COM/network0x0*******************")
+	print("**************************************************************")
+	for i in paketler:
+		os.system("pip install "+i)
+
+		
+		print("*************************************")
+		print("*************************************")
+		print("********** "+ i +" Installed**********") 
+		print("*************************************")
+		print("*************************************")
+	
+if secim == "2":
+	while True:
+		paket = input("Write Package Name  ")
+		os.system("pip install "+paket)
+		print("Press CTRL and C at the same time to exit")
+
+print("********************Completed**************************")
+print("*************INSTAGRAM.COM/network0x0**************************")
+print("************************NetworkSpirit**************************")
